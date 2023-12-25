7aabfc76f241da22e3832945126a5bf0d9a414e2
Salman-080 <salman.niloy552@gmail.com>
2023-12-25 15:23:09 +0600

    added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+// const jwt=require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 
 app.use(cors());
@@ -25,9 +26,12 @@ console.log(process.env.DB_PASS)
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfweqj8.mongodb.net/?retryWrites=true&w=majority`;
 // const uri = "mongodb://localhost:27017";
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfweqj8.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfweqj8.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pcelgh9.mongodb.net/?retryWrites=true&w=majority`;
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,10 +47,13 @@ async function run() {
   try {
 
     console.log("connected success")
+    const categoryCollection = client.db('ProjectDB').collection('CategoryCollections');
     const productsCollection = client.db('ProjectDB').collection('ProductsCollections');
     const cartCollection = client.db('ProjectDB').collection('CartsCollections');
     const reviewCollection = client.db('ProjectDB').collection('ReviewsCollections');
     const userCollection = client.db('ProjectDB').collection('UsersCollections');
+    const orderCollection = client.db('ProjectDB').collection('OrdersCollection');
+
     // Connect the client to the server	(optional starting in v4.7)
 
     //storing products to database
@@ -86,12 +93,47 @@ async function run() {
       const result = await searchResult.toArray();
       res.send(result);
     })
+    //get all products for shop page 
 
-    //get all products for shop page
     app.get("/allProducts", async (req, res) => {
+      const categoryName = req.query.categoryName;
+      console.log(categoryName)
+      if (categoryName && categoryName !== "All") {
+        const query2 = {
+          productCategory: categoryName
+        }
+        const cursor = productsCollection.find(query2);
+        const result = await cursor.toArray();
+        res.send(result);
+
+        return;
+      }
       const cursor = productsCollection.find();
       const result = await cursor.toArray();
+
       res.send(result);
+    })
+
+    //get all products for shop page by category Name
+    app.get("/filterByCategory/:categoryName", async (req, res) => {
+      const categoryName = req.params.categoryName;
+      const query = { productCategory: categoryName };
+      console.log("category", categoryName)
+
+      if (categoryName && categoryName == "All") {
+        const cursor = productsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+      }
+      else {
+        const cursor = productsCollection.find(query);
+        const result = await cursor.toArray();
+
+
+        res.send(result)
+      }
+
+
     })
 
     //individual product fetch by get method
@@ -129,12 +171,12 @@ async function run() {
     })
 
 
-    app.delete("/cartDataDelete/:id", async(req,res)=>{
-      const cardDataId= req.params.id;
-      
-      const query={_id: new ObjectId(cardDataId)};
+    app.delete("/cartDataDelete/:id", async (req, res) => {
+      const cardDataId = req.params.id;
 
-      const result= await cartCollection.deleteOne(query);
+      const query = { _id: new ObjectId(cardDataId) };
+
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
@@ -216,16 +258,384 @@ async function run() {
 
     app.post('/users', async (req, res) => {
       const user = req.body;
-      console.log(user)
+      console.log("new user", user)
       const result = await userCollection.insertOne(user);
+      res.send(result);
     })
 
     //get all users
 
-    app.get("/users", async(req,res)=>{
-      const result= await userCollection.find().toArray();
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
       res.send(result);
     })
+
+
+    //add categories
+
+    app.post("/addCategories", async (req, res) => {
+      const categoryData = req.body;
+
+      const result = await categoryCollection.insertOne(categoryData);
+
+      res.send(result);
+    })
+
+    app.get("/categoriesData", async (req, res) => {
+      const result = await categoryCollection.find().toArray();
+
+      res.send(result);
+    })
+
+    // search products get
+
+    app.get('/searchProducts', async (req, res) => {
+      const searchValue = req.query.searching;
+      console.log("searching", searchValue);
+
+      if (searchValue && searchValue.length !== 0) {
+        const query = {
+          productName: { $regex: searchValue, $options: 'i' }
+        }
+        const result = await productsCollection.find(query).toArray();
+        res.send(result);
+      }
+      else {
+        const result = await productsCollection.find().toArray();
+        res.send(result)
+      }
+
+    })
+
+    //filter by targeter
+
+    // app.get("/targeterFilter/:targeter", async(req,res)=>{
+    //   const targeter= req.params.targeter;
+    //   console.log(targeter);
+
+    //   const query={productTargeter: targeter};
+
+    //   const result= await productsCollection.find(query).toArray();
+    //   res.send(result);
+    // })
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+
+    });
+
+    app.post("/payment", async (req, res) => {
+      const orderInfo = req.body;
+      console.log(orderInfo);
+
+      orderInfo.orderDateTime = new Date();
+
+      const paymentInsResult = await orderCollection.insertOne(orderInfo);
+
+      const productDetailsDec = orderInfo?.itemsInfo?.map(item => (
+        {
+          productId: item?.productId,
+          productSltQnt: item?.productSltQnt
+        }
+      ));
+
+      console.log("product Decrementing", productDetailsDec)
+
+
+      const query = {
+        _id: {
+          $in: orderInfo.cartIds.map(cartId => new ObjectId(cartId))
+        }
+      }
+
+      const deleteFromCartResult = await cartCollection.deleteMany(query);
+
+
+
+      const productQntyDec = productDetailsDec.map(({ productId, productSltQnt }) => ({
+        updateOne: {
+          filter: { _id: new ObjectId(productId) },
+          update: { $inc: { productQuantity: -productSltQnt } }
+        }
+      }));
+
+      // Perform bulk update using updateMany
+      const decmentResult = await productsCollection.bulkWrite(productQntyDec);
+
+      const productQntyDecCart = productDetailsDec.map(({ productId, productSltQnt }) => ({
+        updateOne: {
+          filter: { product_id: productId },
+          update: { $inc: { productQuantity: -productSltQnt } }
+        }
+      }));
+      const decmentResultCart = await cartCollection.bulkWrite(productQntyDecCart);
+
+      const query2 = {
+        productQuantity: { $lte: 0 }
+      }
+
+
+      const removeItemFromCart = await cartCollection.deleteMany(query2);
+      res.send({ paymentInsResult, deleteFromCartResult, decmentResult, decmentResultCart, removeItemFromCart });
+    })
+
+    //managePendings route for admin
+
+    app.get("/managePendingsData", async (req, res) => {
+      const query = {
+        $or: [
+          { paymentStatus: "Pending" },
+          { deliveryStatus: "Pending" }
+        ]
+      }
+      const result = await orderCollection.find(query).toArray();
+
+      res.send(result);
+    })
+
+    //approving payment status by admin
+
+    app.patch("/approvePaymentStatus/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const updateDoc = {
+        $set: {
+          paymentStatus: "Success"
+        },
+      };
+
+      const result = await orderCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
+
+    //approving delivery status by admin
+    app.patch("/approveDeliveryStatus/:id", async (req, res) => {
+      const id = req.params.id;
+      const productsIdSldQny = req?.body?.productsIdSldQny;
+      console.log(productsIdSldQny)
+      console.log(id);
+
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const updateDoc = {
+        $set: {
+          deliveryStatus: "Success"
+        },
+      };
+
+      const result = await orderCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
+
+    // add as admin
+
+    app.patch("/addAsAdmin/:userId", async (req, res) => {
+      const id = req.params.userId;
+      console.log(id);
+
+      const query = {
+        _id: new ObjectId(id)
+      }
+
+      const updateDoc = {
+        $set: {
+          role: "Admin"
+        },
+      };
+
+      const result = await userCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
+
+    // remove from admin 
+    app.patch("/removeFromAdmin/:userId", async (req, res) => {
+      const id = req.params.userId;
+      console.log(id);
+
+      const query = {
+        _id: new ObjectId(id)
+      }
+
+      const updateDoc = {
+        $unset: {
+          role: 1
+        },
+      };
+
+      const result = await userCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
+
+    //displaying all products in Admin side
+
+    app.get("/dashboardAllProducts", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+
+      res.send(result);
+    })
+
+    // admin home stats
+
+    app.get("/adminHomeStats", async (req, res) => {
+      const customers = await userCollection.estimatedDocumentCount()
+      const products = await productsCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            quantity: { $sum: "$productQuantity" },
+          },
+
+        },
+        {
+          $project: {
+            _id: 0,
+            quantity: 1
+          }
+        }
+      ]).toArray();
+      const totalQuantity = products.length > 0 ? products[0].quantity : 0;
+
+      const orders = await orderCollection.estimatedDocumentCount()
+
+      const query = {
+        paymentStatus: "Success"
+      }
+      const payments = await orderCollection.find(query).toArray()
+      console.log(payments)
+      const revenue = payments.reduce((total, payment) => total + payment?.totalPrice, 0);
+
+
+      res.send({ revenue, customers, products: totalQuantity, orders });
+
+    })
+
+    // admin home each category total product counts
+    app.get("/orderStats", async (req, res) => {
+
+      const result = await orderCollection.aggregate([
+        {
+          $match: { paymentStatus: "Success" }
+        },
+        {
+          $unwind: "$itemsInfo"
+        },
+        {
+          $group: {
+            _id: "$itemsInfo.productCategory",
+            totalQuantity: { $sum: "$itemsInfo.productSltQnt" },
+            totalRevenue: {
+              $sum: {
+                $multiply: ["$itemsInfo.productPrice", "$itemsInfo.productSltQnt"]
+              }
+            }
+          }
+        },
+        {
+          $project: {
+            categoryName: "$_id",
+            totalQuantity: 1,
+            totalRevenue: 1,
+            _id: 0
+          }
+        }
+
+      ]).toArray();
+
+      res.send(result);
+    })
+
+    // customer get their order history
+
+    app.get("/usersOrder/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const query = { customerEmail: email };
+
+      const result = await orderCollection.find(query).toArray();
+
+      res.send(result);
+    })
+
+    //payment history admin side count stat
+
+    app.get("/paymentStats", async (req, res) => {
+
+
+      const result = await orderCollection.aggregate([
+        {
+          $facet: {
+            Success: [
+              { $match: { paymentStatus: "Success" } },
+              { $group: { _id: null, count: { $sum: 1 } } },
+              { $project: { _id: 0, count: 1 } },
+            ],
+            Pending: [
+              { $match: { paymentStatus: "Pending" } },
+              { $group: { _id: null, count: { $sum: 1 } } },
+              { $project: { _id: 0, count: 1 } },
+            ],
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            Success: { $arrayElemAt: ["$Success.count", 0] },
+            Pending: { $arrayElemAt: ["$Pending.count", 0] },
+          },
+        },
+      ]).toArray();
+
+      res.send(result)
+    })
+
+    // admin side order history stats
+
+    app.get("/getOrderInfoStat", async(req,res)=>{
+      const deliveryStatus= await orderCollection.aggregate([
+        {
+          $facet: {
+            Success: [
+              { $match: { deliveryStatus: "Success" } },
+              { $group: { _id: null, count: { $sum: 1 } } },
+              { $project: { _id: 0, count: 1 } },
+            ],
+            Pending: [
+              { $match: { deliveryStatus: "Pending" } },
+              { $group: { _id: null, count: { $sum: 1 } } },
+              { $project: { _id: 0, count: 1 } },
+            ],
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            Success: { $arrayElemAt: ["$Success.count", 0] },
+            Pending: { $arrayElemAt: ["$Pending.count", 0] },
+          },
+        },
+      ]).toArray();
+
+      const totalOrders= await orderCollection.estimatedDocumentCount();
+
+      res.send({deliveryStatus, totalOrders});
+    })
+
+
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -236,4 +646,5 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
 
